ebafc3c7ce2f3e90bdab9e644185d4f7d2b57f12
croschel <croschel000@gmail.com>
2020-05-30 20:05:38 -0300

    Navigation and Structure are ready

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './App';
+
+// import { Container } from './styles';
+
+export default function Index() {
+  return <App />;
+}
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function SignIn() {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+export default SignIn;
